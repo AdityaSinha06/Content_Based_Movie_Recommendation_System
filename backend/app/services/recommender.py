9cca1373e523b7d9284cf84552099dca2e2c96af
Aditya Sinha <adityasinha16876@gmail.com>
2026-02-26 02:02:01 +0530

    Changed the recommender.py file to calculate the similarity between the movies upon runtime , rather than calculating everything priorly. This reduced the file size < 100MB.

--- backend/app/services/recommender.py
+++ backend/app/services/recommender.py
@@ -3,11 +3,17 @@
 import os
 import joblib
 import pandas as pd
+import requests
 from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../../"))
-ARTIFACT_DIR = os.path.join(PROJECT_ROOT, "artifacts")
+BACKEND_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../../"))
+ARTIFACT_DIR = os.path.join(BACKEND_ROOT, "artifacts")
 
 
 class ContentRecommender:
@@ -22,6 +28,23 @@ class ContentRecommender:
             index=self.movies["title"].str.lower()
         ).drop_duplicates()
 
+    def fetch_poster(self, title):
+        url = "https://api.themoviedb.org/3/search/movie"
+        params = {
+            "api_key": TMDB_API_KEY,
+            "query": title
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if data["results"]:
+            poster_path = data["results"][0].get("poster_path")
+            if poster_path:
+                return f"https://image.tmdb.org/t/p/w500{poster_path}"
+
+        return None
+
     def recommend(self, movie_name: str, top_n: int = 10):
 
         if not movie_name:
@@ -33,14 +56,9 @@ class ContentRecommender:
             return []
 
         idx = self.indices[movie_name]
-
-        # Get vector of queried movie
         query_vector = self.tfidf_matrix[idx]
 
-        # Compute cosine similarity ON DEMAND
         similarity_scores = cosine_similarity(query_vector, self.tfidf_matrix)[0]
-
-        # Enumerate + sort
         similarity_scores = list(enumerate(similarity_scores))
         similarity_scores = sorted(
             similarity_scores,
@@ -52,11 +70,12 @@ class ContentRecommender:
 
         for i in similarity_scores:
             movie_data = self.movies.iloc[i[0]]
+            title = movie_data["title"]
 
             results.append({
-                "title": movie_data["title"],
-                "poster_url": None,
-                "wikipedia_url": f"https://en.wikipedia.org/wiki/{movie_data['title'].replace(' ', '_')}"
+                "title": title,
+                "poster_url": self.fetch_poster(title),
+                "wikipedia_url": f"https://en.wikipedia.org/wiki/{title.replace(' ', '_')}"
             })
 
         return results
